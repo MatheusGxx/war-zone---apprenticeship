5a5b8421b4ca02fe382360a145ce4eb8edaf9f29
Matheus Galtaroça <111516274+MatheusGxx@users.noreply.github.com>
2024-01-30 14:40:49 -0300

    .

--- frontinterconsulta/src/app/(Initial)/layout.js
+++ frontinterconsulta/src/app/(Initial)/layout.js
@@ -1,15 +1,25 @@
-import '../globals.css'
+import '../globals.css';
 
 export const metadata = {
   title: 'Interconsulta',
   description: 'Interconsulta a melhor plataforma de telemedicina',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <head>
-       <meta name="google-site-verification" content="tlq8ailaemzEIRqWZ7IGaWgEqFTe16r4NQLmNp160wU" />
+        <meta name="google-site-verification" content="tlq8ailaemzEIRqWZ7IGaWgEqFTe16r4NQLmNp160wU" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9BND46GSNV"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-9BND46GSNV');
+          `}
+        </script>
       </head>
       <body className="background">
         <div>
@@ -17,5 +27,5 @@ export default function RootLayout({ children }) {
         </div>
       </body>
     </html>
-  )
-} 
+  );
+}
