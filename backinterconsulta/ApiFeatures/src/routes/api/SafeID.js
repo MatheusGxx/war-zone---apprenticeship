3307d43392bc92f885959c1dc8f10940fb1daa42
Matheus Galtaroça <111516274+MatheusGxx@users.noreply.github.com>
2024-02-02 12:36:35 -0300

    .

--- backinterconsulta/ApiFeatures/src/routes/api/SafeID.js
+++ backinterconsulta/ApiFeatures/src/routes/api/SafeID.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
-import { getSafeId } from '../../services/SafeIdService'
+import { getSafeId } from '../../services/SafeIdService.js'
+
 const router = Router()
 
 router.get('/authorize-safeid', async (req, res) => {
