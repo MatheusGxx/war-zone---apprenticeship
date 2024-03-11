c1944f4a3d468725356ce9de107eadc71d883224
Matheus Galtaroça <111516274+MatheusGxx@users.noreply.github.com>
2024-03-11 18:02:25 -0300

    improve

--- backinterconsulta/ApiFeatures/src/utils/Functions/SavedPacient.js
+++ backinterconsulta/ApiFeatures/src/utils/Functions/SavedPacient.js
@@ -1,31 +1,33 @@
-import { models } from '../../../MongoDB/Schemas/Schemas.js'
-import { customAlphabet } from 'nanoid'
+import { nanoid } from 'nanoid';
+import { ModelRegisterPaciente } from '../../../MongoDB/Schemas/Schemas.js';
 
-const alphabet = '0123456789';
-const generateFourDigitNumber = customAlphabet(alphabet, 4);
+// Generate a random 4-digit number
+const generateFourDigitNumber = () => nanoid(4).split('').join('');
 
-
-export const SavedPaciente = async (CPF) => {
-  try{
-    const getPaciente = await models.ModelRegisterPaciente.findOne({ CPF: CPF})
-    console.log(getPaciente)
-    return getPaciente
-  }catch(e){
-    throw new Error
+export const getSavedPaciente = async (CPF) => {
+  try {
+    const getPaciente = await ModelRegisterPaciente.findOne({ CPF });
+    console.log(getPaciente);
+    return getPaciente;
+  } catch (error) {
+    throw new Error('Error fetching saved paciente');
   }
-}
+};
 
+export const createAccountedPaciente = async (CPF, telefone, nome) => {
+  const newPasswordPaciente = generateFourDigitNumber();
 
-export const CreatedAccountedPaciente = async (CPF,telefone,nome) =>{
-  const newPasswordPaciente = generateFourDigitNumber()
-  const criatedAccountednewPaciente = await new models.ModelRegisterMédico({
-    nome: nome,
-    CPF: CPF,
+  const newPaciente = new ModelRegisterPaciente({
+    nome,
+    CPF,
     senha: newPasswordPaciente,
-    telefone: telefone
-  })
+    telefone,
+  });
 
-  await criatedAccountednewPaciente.save()
-
-  return criatedAccountednewPaciente
-}
+  try {
+    await newPaciente.save();
+    return newPaciente;
+  } catch (error) {
+    throw new Error('Error creating new paciente');
+  }
+};
