1d7a0d5d2e708ba4b7312cf6e8f2ee1756931998
Matheus Galtaroça <111516274+MatheusGxx@users.noreply.github.com>
2024-03-11 18:02:23 -0300

    improve

--- backinterconsulta/ApiFeatures/src/utils/multerIcons.js
+++ backinterconsulta/ApiFeatures/src/utils/multerIcons.js
@@ -3,15 +3,33 @@ import path from 'path'
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "icons/");
+    cb(null, "icons/", (error) => {
+      if (error) {
+        cb(error, null)
+      }
+    })
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
+    cb(null, Date.now() + path.extname(file.originalname), (error) => {
+      if (error) {
+        cb(error, null)
+      }
+    })
   },
 })
 
-const uploadIcons = multer({ storage })
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true)
+  } else {
+    cb(new Error('Invalid file type'), false)
+  }
+}
 
+const limits = {
+  fileSize: 1024 * 1024 * 5 // Limit file size to 5MB
+}
+
+const uploadIcons = multer({ storage, fileFilter, limits })
 
 export default uploadIcons
- 
