e393cc2d9c60d7b7efe2b69c7959204cca54ac01
Matheus Galtaroça <111516274+MatheusGxx@users.noreply.github.com>
2024-03-11 18:02:24 -0300

    improve

--- frontinterconsulta/src/app/(login)/layout.js
+++ frontinterconsulta/src/app/(login)/layout.js
@@ -1,23 +1,23 @@
 'use client'
 import '../globals.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ScriptsSocials } from '../partials/ScriptSocials'
+import ScriptsSocials from '../partials/ScriptSocials'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
 export default function RootLayout({ children }) {
-
   return (
     <html lang="pt-br">
-      <ScriptsSocials/>
+      <head>
+        <ScriptsSocials />
+      </head>
       <body className="background overflow-y-hidden">
         <QueryClientProvider client={queryClient}>
-          <div className="min-h-screen flex flex-col">
-              {children}
-          </div>
+          <div className="min-h-screen flex flex-col">{children}</div>
+          <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
         </QueryClientProvider>
       </body>
     </html>
   )
 }
-
