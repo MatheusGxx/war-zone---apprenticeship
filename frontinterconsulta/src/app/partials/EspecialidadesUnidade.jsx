1472c2d069f0c50c8228c6220cf06595a546e931
Matheus Galtaroça <111516274+MatheusGxx@users.noreply.github.com>
2024-03-11 18:02:31 -0300

    improve

--- frontinterconsulta/src/app/partials/EspecialidadesUnidade.jsx
+++ frontinterconsulta/src/app/partials/EspecialidadesUnidade.jsx
@@ -1,7 +1,9 @@
-export const EspecialidadesUnidades = [
+export const unitSpecialties = [
     'UPA01 - Ortopedista e Traumatologista',
     'UPA01 - Psiquiatria',
     'UPA01 - Ginecologia',
     'UPA01 - Neurocirurgia',
     'UPA01 - Cardiologia'
-]
+] as const;
+
+export type UnitSpecialty = typeof unitSpecialties[number];
