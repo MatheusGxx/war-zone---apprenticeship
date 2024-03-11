a185c186c9a9b1a87192f161ce99129d32541025
Matheus Galtaroça <111516274+MatheusGxx@users.noreply.github.com>
2024-03-11 18:02:27 -0300

    improve

--- frontinterconsulta/src/app/components/ReceitaControlada2.jsx
+++ frontinterconsulta/src/app/components/ReceitaControlada2.jsx
@@ -1,7 +1,10 @@
+import React from 'react';
 
 export const ReceitaControlada2 = () => {
-    return(
-        <>
-        </>
-    )
-}
+  return (
+    <div>
+      <h1>Receita Controlada 2</h1>
+      <p>This is an improved version of the ReceitaControlada2 component.</p>
+    </div>
+  );
+};
