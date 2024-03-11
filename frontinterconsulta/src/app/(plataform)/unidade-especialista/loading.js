1e6662bf37c61f0a990417b1dfb53a64f87e8dc8
Matheus Galtaroça <111516274+MatheusGxx@users.noreply.github.com>
2024-03-11 18:02:29 -0300

    improve

--- frontinterconsulta/src/app/(plataform)/unidade-especialista/loading.js
+++ frontinterconsulta/src/app/(plataform)/unidade-especialista/loading.js
@@ -1,9 +1,29 @@
 import PreloaderInterno from '../../partials/PreloaderInterno'
 
-const Loading = () =>{
-  return(
-   <PreloaderInterno/>
+const Loading = () => {
+  return (
+    <PreloaderInterno />
   )
 }
 
-export default Loading
+// Add a message prop to customize the loading message
+Loading.propTypes = {
+  message: PropTypes.string
+}
+
+Loading.defaultProps = {
+  message: 'Loading...'
+}
+
+// Add a message to the loading component
+const LoadingWithMessage = ({ message }) => {
+  return (
+    <div>
+      {message}
+      <PreloaderInterno />
+    </div>
+  )
+}
+
+// Use LoadingWithMessage instead of Loading when you want to customize the message
+export { Loading, LoadingWithMessage }
