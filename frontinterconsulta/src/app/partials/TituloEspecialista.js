26e116043704eba06b73a039e063cc9a51bf06b6
Matheus Galtaroça <111516274+MatheusGxx@users.noreply.github.com>
2024-03-11 18:02:30 -0300

    improve

--- frontinterconsulta/src/app/partials/TituloEspecialista.js
+++ frontinterconsulta/src/app/partials/TituloEspecialista.js
@@ -1,4 +1,10 @@
-export const TitutloEspecialista = [
+export const TITLES_SPECIALIST = [
   'Dr.',
   'Dra.'
-]
+];
+
+// or if you prefer using enum
+export enum TitlesSpecialist {
+  DR = 'Dr.',
+  DRA = 'Dra.'
+}
