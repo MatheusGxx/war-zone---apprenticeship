07c5645968f8634a153bfa851c5f551d35923475
Matheus Galtaroça <111516274+MatheusGxx@users.noreply.github.com>
2024-03-11 18:02:24 -0300

    improve

--- backinterconsulta/ApiFeatures/src/utils/multerPlanilha.js
+++ backinterconsulta/ApiFeatures/src/utils/multerPlanilha.js
@@ -1,17 +1,20 @@
 import multer from 'multer';
 import path from 'path';
+import { v4 as uuidv4 } from 'uuid'; // Generate unique IDs
 
+// Set up Multer storage configuration
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "planilhas/"); // Pasta onde os arquivos serão salvos
+    cb(null, 'planilhas/'); // Set the destination directory
   },
   filename: (req, file, cb) => {
-    const extname = path.extname(file.originalname);
-    const filename = Date.now() + extname;
+    const extname = path.extname(file.originalname); // Get file extension
+    const filename = `${uuidv4()}${extname}`; // Generate unique filename with extension
     cb(null, filename);
   },
 });
 
+// Create Multer instance with the storage configuration
 const uploadPlanilha = multer({ storage });
 
-export default uploadPlanilha
+export default uploadPlanilha;
